b3e5dabfa6da3991fe3aec060cf5da31efa0570d
david <github@activecq.com>
2012-11-14 00:04:24 -0500

    Updated copyright text.

--- core/src/test/java/com/activecq/api/utils/TextUtilTest.java
+++ core/src/test/java/com/activecq/api/utils/TextUtilTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 david.
+ * Copyright 2012 david gonzalez.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
